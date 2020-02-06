76e95f66ff9497d45782d7464aa03883c1fba28f
bahadircimen <bahadircimn@gmail.com>
2020-02-06 13:12:00 +0300

    change dynamic rows add css

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,26 +3,65 @@ import styles from './styles.scss';
 
 
 class Table extends Component {
-    render() {
-        const {id,name,age,email}=this.props;
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: 'Wasif', age: 21, email: 'wasif@email.com' },
+                { name: 'Ali', age: 19, email: 'ali@email.com' },
+                { name: 'Saad', age: 16, email: 'saad@email.com' },
+                { name: 'Asad', age: 25, email: 'asad@email.com' },
+                { name: 'alpkan', age: 21, email: 'wasif@email.com' },
+                { name: 'barış', age: 19, email: 'ali@email.com' },
+                { name: 'merve', age: 16, email: 'saad@email.com' },
+                { name: 'bahadır', age: 25, email: 'asad@email.com' },
+                { name: 'kenan', age: 21, email: 'wasif@email.com' },
+                { name: 'tanzer', age: 19, email: 'ali@email.com' },
+                { name: 'ruken', age: 16, email: 'saad@email.com' },
+                { name: 'berk', age: 25, email: 'asad@email.com' },
+                { name: 'serkan', age: 25, email: 'asad@email.com' },
 
+            ]
+        }
+    }
+
+    getKeys = () =>{
+        return Object.keys(this.state.data[0]);
+    };
+
+    getHeader = () =>{
+        let keys = this.getKeys();
+        return keys.map((key, index)=>{
+            return <th key={key}>{key.toUpperCase()}</th>
+        })
+    };
+
+    tableData() {
+        return this.state.data.map((d, index) => {
+            return (
+                <tr key={index}>
+                    <td>{d.name}</td>
+                    <td>{d.age}</td>
+                    <td>{d.email}</td>
+                    <td>{index}</td>
+                </tr>
+            )
+        })
+    }
+
+
+    render() {
         return (
             <div>
                 <table>
-                    <tr>
-                        <td>
-                            {id}
-                        </td>
-                        <td>
-                            {name}
-                        </td>
-                        <td>
-                            {age}
-                        </td>
-                        <td>
-                            {email}
-                        </td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            {this.getHeader()}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.tableData()}
+                    </tbody>
                 </table>
             </div>
         );
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,60 +3,15 @@ import Table from "../components/Table";
 import styles from "./styles.scss";
 
 class MainContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [
-				{ name: 'Wasif', age: 21, email: 'wasif@email.com' },
-				{ name: 'Ali', age: 19, email: 'ali@email.com' },
-				{ name: 'Saad', age: 16, email: 'saad@email.com' },
-				{ name: 'Asad', age: 25, email: 'asad@email.com' },
-				{ name: 'alpkan', age: 21, email: 'wasif@email.com' },
-				{ name: 'barış', age: 19, email: 'ali@email.com' },
-				{ name: 'merve', age: 16, email: 'saad@email.com' },
-				{ name: 'bahadır', age: 25, email: 'asad@email.com' },
-				{ name: 'kenan', age: 21, email: 'wasif@email.com' },
-				{ name: 'tanzer', age: 19, email: 'ali@email.com' },
-				{ name: 'ruken', age: 16, email: 'saad@email.com' },
-				{ name: 'berk', age: 25, email: 'asad@email.com' },
-				{ name: 'serkan', age: 25, email: 'asad@email.com' },
-
-			]
-		}
-	}
-	renderTableHeader=()=> {
-		let header = Object.keys(this.state.data[0]);
-		 return header.map((key, index) => {
-			return <th key={index}>{key.toUpperCase()}</th>
-		})
-	};
-
 	render() {
-		const {data} = this.state;
 		return (
 			<Fragment>
 				<div className={styles.container}>
 					<div className={styles.row}>
-						<div className={styles.colMd2}/>
-						<tr>{this.renderTableHeader()}</tr>
-					</div>
-						<div className={styles.colMd8}>
-							{
-								data.map((d,index) => {
-									return (
-										<div key={index}
-											 className={styles.colMd8}>
-											<Table
-											id={index}
-											name={d.name}
-											age={d.age}
-											email={d.email}
-											/>
-										</div>
-									)
-								})
-							}
+						<div className={styles.colMd9}>
+							<Table/>
 						</div>
+					</div>
 				</div>
 			</Fragment>
 		);
