a0fb4a1be497ef7264bf86a85b7b1072e2ab007f
bahadircimen <bahadircimn@gmail.com>
2020-02-09 14:48:09 +0300

    add (dynamic column, show entries)

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import styles from './styles.scss';
 
+const RenderData = (state) =>{
+    return state.keys.map((key)=>{
+        return <td key={state.data[key]}>{state.data[key]}</td>
+    })
+};
 
 class Table extends Component {
     constructor(props) {
@@ -21,22 +26,35 @@ class Table extends Component {
                 { name: 'berk', age: 25, email: 'asad@email.com' },
                 { name: 'serkan', age: 25, email: 'asad@email.com' },
 
-            ]
+            ],
         }
     }
 
-    getKeys = () =>{
+    /*****dynamic   column 2**/
+    tableValues = () => {
+            return Object.values(this.state.data);
+    };
+
+    tableA = () => {
+        return this.tableValues().map((d,index) => {
+            return (
+                    <td key={index}>{d}</td>
+            )
+        })
+    };
+
+    tableKeys = () => {
         return Object.keys(this.state.data[0]);
     };
 
-    getHeader = () =>{
-        let keys = this.getKeys();
-        return keys.map((key, index)=>{
+    tableHeader = () => {
+        return this.tableKeys().map((key) => {
             return <th key={key}>{key.toUpperCase()}</th>
         })
     };
 
-    tableData() {
+    /**manuel column
+     tableData = () => {
         return this.state.data.map((d, index) => {
             return (
                 <tr key={index}>
@@ -47,22 +65,47 @@ class Table extends Component {
                 </tr>
             )
         })
-    }
+    }; */
 
+    tableData = () => {
+        return this.state.data.map((d, index)=>{
+            return <tr key={index}>
+                <RenderData key={index} data={d} keys={this.tableKeys()} />
+            </tr>
+        })
+    };
+
+    /****tableB = () => {
+        return this.state.data.map((d,index)=> {
+            return (
+                <tr key={index}>
+                    <td>{this.tableKeys().map}</td>
+                </tr>
+            )
+        })
+    };
+*/
 
     render() {
+        console.log(this.tableA());
         return (
             <div>
                 <table>
                     <thead>
                         <tr>
-                            {this.getHeader()}
+                            {this.tableHeader()}
                         </tr>
                     </thead>
                     <tbody>
                         {this.tableData()}
                     </tbody>
+                    <tfoot>
+                    <tr>
+                        {this.tableHeader()}
+                    </tr>
+                    </tfoot>
                 </table>
+                    Showing {this.state.data.length} entries.
             </div>
         );
     }
